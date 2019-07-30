8ca5419f2f081a2211d24470bc298b826d56cd66
Yahiko <rieffelkai@gmail.com>
2019-07-30 15:17:26 -0400

    Update LandingCards.jsx

--- src/Components/Landing/LandingCards.jsx
+++ src/Components/Landing/LandingCards.jsx
@@ -7,13 +7,13 @@ export const LandingCards = () => (
 		<div className='container'>
 			<div className='row'>
 				<div className='col-sm-4'>
-					<Card title='Evaluate' />
+					<Card title='Evaluate' subtitle='Before stepping into solving a problem, careful evaluation is required to ensure the best solution.' />
 				</div>
 				<div className='col-sm-4'>
-					<Card title='Develop' />
+					<Card title='Develop' subtitle='Ideas must be fully developed before they can become potential solutions. Trial and error is applied with past experience to maximize the outcome.' />
 				</div>
 				<div className='col-sm-4'>
-					<Card title='Redefine' />
+					<Card title='Redefine' subtitle='Solutions redefine old standards and broaden horizons. They don\'t solve anything if they don\'t change anything.' />
 				</div>
 			</div>
 		</div>
