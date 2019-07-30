51ed1372c44e1107fb423528c32a65cfcb246f41
Yahiko <rieffelkai@gmail.com>
2019-07-30 15:34:40 -0400

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import { Navbar } from './Layout/Navbar';
 import { Footer } from './Layout/Footer';
@@ -16,7 +16,7 @@ export const App = () => (
 				<Route exact path='/' component={Landing} />
 				<Route exact path='/about' component={About} />
 				<Route exact path='/projects' component={Projects} />
-				<Route component={() => (window.location.href = '/')} />
+				<Route component={() => <Redirect to='/'>} />
 			</Switch>
 			<Footer />
 		</Router>
